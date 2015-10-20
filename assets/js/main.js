093b685ef8f5292cf3bbfb8f6f0364e942f61ed5
mumunana <329170831@qq.com>
2015-10-20 17:34:35 +0800

    根据原型改造

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 require.config({
-  baseUrl: "assets/js/lib/"
-  , shim: {
-    'backbone': {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    },
-    'underscore': {
-      exports: '_'
-    },
-    'bootstrap': {
-      deps: ['jquery'],
-      exports: '$.fn.popover'
+    baseUrl: "assets/js/lib/"
+    , shim: { //shim  用来配置不符合AMD规范的模块 比如  underscore.js  和 Backbone.js
+        'backbone': {
+            deps: ['underscore', 'jquery'], // backbone 的依赖（1）exports值（输出的变量名），表明这个模块外部调用时的名称；（2）deps数组，表明该模块的依赖性。
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: '$.fn.popover'
+        }
     }
-  }
-  , paths: {
-    app         : ".."
-    , collections : "../collections"
-    , data        : "../data"
-    , models      : "../models"
-    , helper      : "../helper"
-    , templates   : "../templates"
-    , views       : "../views"
-  }
+    , paths: {
+        app: ".."
+        , collections: "../collections"
+        , data: "../data"
+        , helper: "../helper"
+        , models: "../models"
+        , templates: "../templates"
+        , views: "../views"
+    }
 });
-require([ 'app/app'], function(app){
-  app.initialize();
+require(['app/app'], function (app) {
+    app.initialize();
 });
